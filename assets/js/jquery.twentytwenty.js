e0af79b13baa633bdc1c2d375f0a0347100d5e53
ezzi97 <ezeddin.eddaouy@gmail.com>
2020-11-16 17:38:30 +0100

    aggiornamento twenty-twenty

--- assets/js/jquery.twentytwenty.js
+++ assets/js/jquery.twentytwenty.js
@@ -39,8 +39,8 @@
       afterImg.addClass("twentytwenty-after");
 
       var calcOffset = function(dimensionPct) {
-        var w = beforeImg.width();
-        var h = beforeImg.height();
+        var w = beforeImg.actual("width");
+        var h = beforeImg.actual("height");
         return {
           w: w+"px",
           h: h+"px",

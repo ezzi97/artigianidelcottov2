5b91ed08db531643577b4127af99a76348fd7bc0
ezzi97 <ezeddin.eddaouy@gmail.com>
2020-12-02 18:44:18 +0100

    aggiornamento nodemailer - fix bug

--- classes/Mailer.js
+++ classes/Mailer.js
@@ -9,8 +9,8 @@ class Mailer {
           host: "smtp.gmail.com",
           service: "gmail",
           auth: {
-            user: ""+process.env.GMAIL_USER,
-            pass: ""+process.env.GMAIL_PASS
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_PASS
           }
       }));
     }
@@ -19,12 +19,14 @@ class Mailer {
       var smtpSender = this.smtpTrans;
       this.sendToAdminEmail(params, function(ris) {
         if (ris.statusCode === 200) {
+          console.log("rendering ejs file!!");
           ejs.renderFile(__dirname + "/email.ejs", { nome: params.name }, function (err, data) {
             if (err) {
               console.log(err);
               callback({error: err, statusCode: 403});
               return;
             } else {
+              console.log("im here!!");
                 smtpSender.sendMail({from: ''+process.env.GMAIL_USER, to: ""+parametri.email, subject: 'No-Reply Artigianidelcotto', html: data}, (error, response) => {
                     if (error) {
                       callback({error: error, statusCode: 403});
@@ -39,12 +41,14 @@ class Mailer {
             });
         }
         else {
+          console.log(ris.error);
           callback({error: ris.error, statusCode: ris.statusCode});
           return;
         }
       });
     }
     sendToAdminEmail(params, callback) {
+      console.log(params);
       this.mailOpts = {
         from: ''+params.email, // This is ignored by Gmail
         to: ''+process.env.GMAIL_USER,

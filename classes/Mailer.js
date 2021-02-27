cf31047abbe0630b267996c934ca4a4abfdb0995
Ezzi <ezeddin.eddaouy@gmail.com>
2021-02-27 16:49:02 +0100

    aggiornamento sito web generale

--- classes/Mailer.js
+++ classes/Mailer.js
@@ -25,8 +25,8 @@ class Mailer {
           }
         });*/
       const msg = {
-        to: 'test@example.com', // Change to your recipient
-        from: 'test@example.com', // Change to your verified sender
+        to: 'ezeddin.eddaouy@gmail.com', // Change to your recipient
+        from: 'trucesdin@gmail.com', // Change to your verified sender
         subject: 'Sending with SendGrid is Fun',
         text: 'and easy to do anywhere, even with Node.js',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
